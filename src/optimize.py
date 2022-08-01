a4301d95f8727d7a4c5175a994cad0054f082455
BARRY-THINKPAD\barry <b.e.r.terheegde@student.utwente.nl>
2022-08-01 16:16:14 +0200

    bugfix

--- src/optimize.py
+++ src/optimize.py
@@ -3,16 +3,19 @@ import numpy as np
 
 
 random_booking = bu.generate_bookings()
+
+# for group in random_booking:
+#     print(group)
+
 bu.show_bookings(random_booking)
+
 
 grouped_scheme = bu.group_bookings(bu.ungroup_bookings(random_booking))
 # grouped_scheme = bu.generate_grouped_bookings()
-
 # print(grouped_scheme)
 
 grouped_scheme.sort(key=lambda tup: tup[0])
-# for group in grouped_scheme:
-#     print(group)
+
 
 
 
@@ -21,7 +24,7 @@ def assign_room(bookings):
     for group in bookings:
         r = 0
         while True:
-            if (len(planning[r]) < 1) or (group[0][0] > planning[r][-1][1]):
+            if (len(planning[r]) < 1) or (group[0][0] >= planning[r][-1][1]):
                 planning[r].extend(group)
                 break
             r += 1
@@ -31,8 +34,8 @@ def assign_room(bookings):
 
 new_scheme = assign_room(grouped_scheme)
 
-# for room in new_scheme:
-#     print(room, len(room))
+for room in new_scheme:
+    print(room, len(room))
 
 bu.show_bookings(new_scheme)
 
