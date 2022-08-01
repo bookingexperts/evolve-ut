301fa3618e281a92d0eb8d8aa60dc8892ba44a0e
BARRY-THINKPAD\barry <b.e.r.terheegde@student.utwente.nl>
2022-08-01 15:21:33 +0200

    basic optimize version with visuals

--- src/optimize.py
+++ src/optimize.py
@@ -1,24 +1,27 @@
 import booking_utils as bu
 import numpy as np
 
-grouped_scheme = bu.generate_grouped_bookings()
 
-print(grouped_scheme)
+random_booking = bu.generate_bookings()
+bu.show_bookings(random_booking)
 
+grouped_scheme = bu.group_bookings(bu.ungroup_bookings(random_booking))
+# grouped_scheme = bu.generate_grouped_bookings()
+
+# print(grouped_scheme)
 
 grouped_scheme.sort(key=lambda tup: tup[0])
-for group in grouped_scheme:
-    print(group)
+# for group in grouped_scheme:
+#     print(group)
 
 
 
 def assign_room(bookings):
     planning = [[], [], [], [], []]
-    print(planning)
     for group in bookings:
         r = 0
         while True:
-            if (len(planning[r]) < 1) or (group[0][0] > planning[r][-1][0]):
+            if (len(planning[r]) < 1) or (group[0][0] > planning[r][-1][1]):
                 planning[r].extend(group)
                 break
             r += 1
@@ -28,9 +31,10 @@ def assign_room(bookings):
 
 new_scheme = assign_room(grouped_scheme)
 
-for room in new_scheme:
-  #  print(room, len(room))
-    bu.show_bookings(room)
+# for room in new_scheme:
+#     print(room, len(room))
+
+bu.show_bookings(new_scheme)
 
 
 
