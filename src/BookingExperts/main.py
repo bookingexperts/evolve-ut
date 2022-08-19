69decbd2d46f4caa5657477e230ff86960ab26ef
Mathijs-Vogelezang <70201164+Mathijs-Vogelezang@users.noreply.github.com>
2022-08-19 10:56:35 +0200

    send to server

--- src/BookingExperts/main.py
+++ src/BookingExperts/main.py
@@ -5,11 +5,12 @@ import time
 from evaluation_booking import evaluate, visualize, visualize_original_graph
 from src.BookingExperts.bruteforce import bruteforce
 from src.BookingExperts.data.server_communication import get_bookings, get_rentables, get_rentable_types, \
-    filter_bookings_on_type, filter_rentables_on_type
+    filter_bookings_on_type, filter_rentables_on_type, update_multiple_booking_rentables
 from vns import variable_neighbourhood_search
 
 
 def run_vns(bookings):
+    begin_time = time.time()
     for rentable_type in get_rentable_types():
         bookings_by_type = filter_bookings_on_type(rentable_type, bookings)
         # visualize_original_graph(bookings_by_type)
@@ -17,9 +18,11 @@ def run_vns(bookings):
         gaps, max_gap = evaluate(bookings_by_type_dict)
         print(bookings_by_type_dict)
         heuristic_gapcount, heuristic_max_gap, heuristic_bookings = variable_neighbourhood_search(10, gaps, max_gap, bookings_by_type_dict)
-
         # heuristic_bookings = bruteforce(bookings_by_type)
+        total_time = time.time() - start_time
         visualize(heuristic_bookings)
+        print(f"Computation time for type {rentable_type}:", round(total_time, 2), "seconds")
+        update_multiple_booking_rentables(list(heuristic_bookings.values()))
 
 if __name__ == "__main__":
     start_time = time.time()
