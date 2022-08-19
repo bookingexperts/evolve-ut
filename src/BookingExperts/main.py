ae71e1c7ca1da2a2da0f185d7f56cf0e330ed960
BARRY-THINKPAD\barry <b.e.r.terheegde@student.utwente.nl>
2022-08-19 10:59:19 +0200

    final2

--- src/BookingExperts/main.py
+++ src/BookingExperts/main.py
@@ -10,16 +10,16 @@ from vns import variable_neighbourhood_search
 
 
 def run_vns(bookings):
-    begin_time = time.time()
     for rentable_type in get_rentable_types():
+        begin_time = time.time()
         bookings_by_type = filter_bookings_on_type(rentable_type, bookings)
         # visualize_original_graph(bookings_by_type)
         bookings_by_type_dict = { booking.res_id : booking for booking in bookings_by_type }
         gaps, max_gap = evaluate(bookings_by_type_dict)
-        print(bookings_by_type_dict)
+
         heuristic_gapcount, heuristic_max_gap, heuristic_bookings = variable_neighbourhood_search(10, gaps, max_gap, bookings_by_type_dict)
         # heuristic_bookings = bruteforce(bookings_by_type)
-        total_time = time.time() - start_time
+        total_time = time.time() - begin_time
         visualize(heuristic_bookings)
         print(f"Computation time for type {rentable_type}:", round(total_time, 2), "seconds")
         update_multiple_booking_rentables(list(heuristic_bookings.values()))
